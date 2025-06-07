0caeaead5d8e7cb17929e862408a637bdefc533a
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2025-06-07 22:44:08 +0530

    Time: 7 ms (96.55%) | Memory: 44.8 MB (70.96%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-    int n=s.length();
-    int m=p.length();
-    if(n<m) return new ArrayList<>();
-    int ch[]=new int[26];
-    ArrayList<Integer> ag=new ArrayList<>();
-    for(int i=0;i<m;i++){
-        char c1=s.charAt(i);
-        char c2=p.charAt(i);
-        ch[c2-'a']++;
-        ch[c1-'a']--;
-    }
-   
-    if(allZero(ch)) ag.add(0);
-    int track=0;
-    for(int i=m;i<n;i++){
+        int ch[]=new int[26];
+        int n=s.length();
+        int m=p.length();
+        if(m>n){
+            return new ArrayList<>();
+        }
+        List<Integer> al=new ArrayList<>();
+        for(int i=0;i<m;i++){
+            ch[p.charAt(i)-'a']++;
+            ch[s.charAt(i)-'a']--;
+        }
+        if(allZero(ch)) al.add(0);
+        for(int i=m;i<n;i++){
             ch[s.charAt(i)-'a']--;
             ch[s.charAt(i-m)-'a']++;
-            if(!allZero(ch)){
-                
-                track=i;
-            }
             if(allZero(ch)){
-                track=i-m+1;
-                ag.add(track);
+                al.add(i-m+1);
             }
         }
-    return ag;
+        return al;
     }
-    public boolean allZero(int ch[]){
-        for(int n:ch){
+    public boolean allZero(int a[]){
+        for(int n: a){
             if(n!=0){
                 return false;
             }
