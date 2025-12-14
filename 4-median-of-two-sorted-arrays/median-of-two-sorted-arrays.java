8e04459d5e1272e61d3ef795ace049541a9c80b3
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2025-12-14 18:46:49 +0530

    Time: 2 ms (47.13%) | Memory: 49.1 MB (7.39%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,42 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       int merged[]= merge(nums1,nums2);
-       int n=merged.length;
-       if(n%2==0){
-            return (merged[n/2-1] +merged[n/2])/2.00000;
-       }
-       else{
-        return merged[n/2];
-       }
+        int ans[]=merge(nums1,nums2);
+        int n=ans.length;
+        if(n%2==0){
+            return (ans[n/2-1]+ans[n/2])/2.0;
+        }
+        else{
+            return ans[n/2];
+        }
     }
-    
-    public int[] merge(int[] num1,int[] num2){
-        int n=num1.length;
-        int m=num2.length;
-        int merged[]=new int[n+m];
+    public int[] merge(int nums1[],int nums2[]){
+        int n=nums1.length;
+        int m=nums2.length;
         int i=0,j=0,k=0;
+        int ans[]=new int[n+m];
         while(i<n && j<m){
-            if(num1[i]<num2[j]){
-                merged[k]=num1[i];
+            if(nums1[i]<nums2[j]){
+                ans[k]=nums1[i];
                 i++;
                 k++;
             }
             else{
-                merged[k]=num2[j];
+                ans[k]=nums2[j];
                 j++;
                 k++;
             }
         }
         while(i<n){
-            merged[k]=num1[i];
-                i++;
-                k++;
+            ans[k]=nums1[i];
+            i++;
+            k++;
         }
         while(j<m){
-            merged[k]=num2[j];
-                j++;
-                k++;
+            ans[k]=nums2[j];
+            j++;
+            k++;
         }
-        return merged;
+        return ans;
     }
 }
