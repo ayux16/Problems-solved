e45c35abdbda5d92ce72f618455e6e803155d413
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-03-13 10:54:54 +0530

    Time: 6 ms (42.89%) | Memory: 44 MB (98.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,7 +3,7 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char ch[]=new char[26];
+        int ch[]=new int[26];
         for(int i=0;i<s.length();i++){
             ch[t.charAt(i)-'a']--;
             ch[s.charAt(i)-'a']++;
@@ -13,8 +13,8 @@ class Solution {
         }
         return true;
     }
-    public boolean check(char ch[]){
-        for(char c: ch){
+    public boolean check(int ch[]){
+        for(int c: ch){
             if(c!=0){
                 return false;
             }
