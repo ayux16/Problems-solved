5251bf825212f09b44d21489617501fc1551b266
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-04-24 15:39:24 +0530

    Time: 8 ms (53.23%) | Memory: 44.2 MB (51.71%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n=s1.length();
-        int m=s2.length();
-        if(n>m) return false;
-        int ans[]=new int[26];
-
-        for(int i=0;i<n;i++){
-            ans[s1.charAt(i)-'a']++;
-            ans[s2.charAt(i)-'a']--;
+        if(s2.length()<s1.length()){
+            return false;
         }
-        if(check(ans)) return true;
-        for(int i=n;i<m;i++){
-            ans[s2.charAt(i)-'a']--;
-            ans[s2.charAt(i-n)-'a']++;
+        int ans[]=new int[26];
+        for(int i=0;i<s1.length();i++){
+            ans[s1.charAt(i)-'a']--;
+            ans[s2.charAt(i)-'a']++;
+        }
+        if(check(ans)){return true;}
+        for(int i=s1.length();i<s2.length();i++){
+            ans[s2.charAt(i)-'a']++;
+            ans[s2.charAt(i-s1.length())-'a']--;
             if(check(ans)){return true;}
         }
         return false;
     }
     public boolean check(int ans[]){
-        for(int n:ans){ 
-            if(n!=0){return false;}
+        for(int n: ans){
+            if(n!=0){
+                return false;
+            }
         }
         return true;
     }
