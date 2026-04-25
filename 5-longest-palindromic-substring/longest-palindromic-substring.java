061d4705583e68b7f6dd14266d2d0f860bb55c35
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-04-25 11:46:38 +0530

    Time: 13 ms (95.97%) | Memory: 47.1 MB (14.49%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,22 +1,24 @@
 class Solution {
-    String ans="";
+    String ans = "";
+
     public String longestPalindrome(String s) {
-        int n=s.length();
-        check(s,0,n);
-        check(s,1,n);
+        int n = s.length();
+        check(s, 0, n);
+        check(s, 1, n);
         return ans;
     }
-    public void check(String s, int x, int n){
-        for(int e=0;e<n;e++){
-            int i=e;
-            int j=e+x;
-            while(i>=0 && j<n && s.charAt(i)==s.charAt(j)){
+
+    public void check(String s, int x, int n) {
+        for (int e = 0; e < n; e++) {
+            int i = e;
+            int j = e + x;
+            while (i >= 0 && j < n && s.charAt(i) == s.charAt(j)) {
                 j++;
                 i--;
             }
-            String sub=s.substring(i+1,j);
-            if(sub.length()>ans.length()){
-                ans=sub;
+            String sub = s.substring(i + 1, j);
+            if (sub.length() > ans.length()) {
+                ans = sub;
             }
         }
     }
