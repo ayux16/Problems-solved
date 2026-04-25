5107533cb0c0d847e883284fcb13c01e3a87c9c7
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-04-25 11:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (76.37%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.java
+++ 1252-break-a-palindrome/break-a-palindrome.java
@@ -1,14 +1,17 @@
 class Solution {
-    public String breakPalindrome(String pal) {
-        if(pal.length()==1) return "";
-        char ch[]=pal.toCharArray();
-        for(int i=0;i<pal.length()/2;i++){
+    public String breakPalindrome(String palindrome) {
+        int n=palindrome.length();
+        if(n==1){
+            return "";
+        }
+        char ch[]=palindrome.toCharArray();
+        for(int i=0;i<n/2;i++){
             if(ch[i]!='a'){
                 ch[i]='a';
                 return new String(ch);
             }
         }
-        ch[pal.length()-1]='b';
+        ch[n-1]='b';
         return new String(ch);
     }
 }
