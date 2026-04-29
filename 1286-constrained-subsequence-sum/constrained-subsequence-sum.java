73a7e4191216db3698cd8a7b73b5cb0a747695de
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-04-29 15:44:14 +0530

    Time: 35 ms (67.28%) | Memory: 112.9 MB (69.11%) - LeetSync

---
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int constrainedSubsetSum(int[] nums, int k) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        Deque<Integer> dq=new ArrayDeque<>();
+        dq.addLast(0);
+        int ans=nums[0];
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            while(!dq.isEmpty() && dq.peekFirst()<i-k){
+                dq.removeFirst();
+            }
+            dp[i]=nums[i]+Math.max(0,dp[dq.peekFirst()]);
+            ans=Math.max(ans,dp[i]);
+            while(!dq.isEmpty() && dp[dq.peekLast()]<=dp[i]){
+                dq.pollLast();
+            }
+            dq.addLast(i);
+        }
+        return ans;
+    }
+}
