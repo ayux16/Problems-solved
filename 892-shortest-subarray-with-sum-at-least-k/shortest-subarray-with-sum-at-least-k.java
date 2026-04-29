bc31ae9290d6eb2ddd98426f3d161c1111b5611b
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-04-29 14:39:08 +0530

    Time: 26 ms (95.17%) | Memory: 92.4 MB (55.09%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -3,20 +3,19 @@ class Solution {
         Deque<Integer> dq=new ArrayDeque<>();
         int n=nums.length;
         long pf[]=new long[n+1];
-        int len=Integer.MAX_VALUE;
+        int ans=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
             pf[i+1]=pf[i]+nums[i];
         }
-
         for(int i=0;i<=n;i++){
-            while(!dq.isEmpty() && pf[i]-pf[dq.peekFirst()] >=k){
-                len=Math.min(len,i-dq.pollFirst());
+            while(!dq.isEmpty() && pf[i]-pf[dq.peekFirst()]>=k){
+                ans=Math.min(ans,i-dq.pollFirst());
             }
             while(!dq.isEmpty() && pf[i]<=pf[dq.peekLast()]){
                 dq.pollLast();
             }
             dq.addLast(i);
         }
-        return len==Integer.MAX_VALUE?-1:len;
+        return ans==Integer.MAX_VALUE?-1:ans;
     }
 }
