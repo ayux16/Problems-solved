66d627ca989d0db46d7c68f97f719afd88d64006
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-06-22 14:48:40 +0530

    Time: 11 ms (95.98%) | Memory: 51.4 MB (5.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer> freq[]=new List[nums.length+1];
-        Map<Integer,Integer> hm=new HashMap<>();
-
-        for(int i=0;i<freq.length;i++){
-            freq[i]=new ArrayList<>();
+        int x=nums.length;
+        List<Integer> freq[]=new List[x+1];
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<x;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
 
-        for(int n: nums){
-            hm.put(n,hm.getOrDefault(n,0)+1);
+        for(int i=0;i<=x;i++){
+            freq[i]=new ArrayList<>();
         }
 
         for(Map.Entry<Integer,Integer> entry: hm.entrySet()){
             freq[entry.getValue()].add(entry.getKey());
         }
-        
         int j=0;
         int ans[]=new int[k];
         for(int i=freq.length-1;i>=0 && j<=k;i--){
-            for(int n: freq[i]){
+            for(int n:freq[i]){
                 ans[j++]=n;
                 if(j==k){
                     return ans;
