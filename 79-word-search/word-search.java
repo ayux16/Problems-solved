550796a91b25b07e81de473445c3330bd221ea46
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-07-03 14:26:56 +0530

    Time: 130 ms (73.49%) | Memory: 42.8 MB (75.20%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,5 +1,4 @@
 class Solution {
-    boolean flag=false;
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
@@ -7,36 +6,35 @@ class Solution {
         int k=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]!=word.charAt(0)){
-                    continue;
-                }
-                else{
-                    check(board,word,i,j,0,visited);
+                if(board[i][j]==word.charAt(0) && check(board,word,i,j,0,visited) ){
+                    return true;
                 }
             }
         }
-        return flag;
+        return false;
     }
-public void check(char[][] board, String word,int i,int j,int k,boolean visited[][]){
+public boolean check(char[][] board, String word,int i,int j,int k,boolean visited[][]){
         
-        if(i<0 || j<0 || i>board.length-1 || j>board[0].length-1){
-            return ;
-        }
-        if(board[i][j]!=word.charAt(k)){
-            return;
-        } 
-        if(visited[i][j]){
-            return;
+        if(i<0 || j<0 
+            || i>board.length-1 
+            || j>board[0].length-1 
+            || board[i][j]!=word.charAt(k) 
+            || visited[i][j]
+        ){
+            return false;
         }
         if(k==word.length()-1){
-            flag=true;
-            return;
+           return true;
         }
         visited[i][j]=true;
-        check(board,word,i-1,j,k+1,visited); //up
-        check(board,word,i,j-1,k+1,visited); //down
-        check(board,word,i+1,j,k+1,visited); //left
-        check(board,word,i,j+1,k+1,visited); //right
+        if(check(board,word,i-1,j,k+1,visited) || //up
+           check(board,word,i,j-1,k+1,visited) || //down
+           check(board,word,i+1,j,k+1,visited) || //left
+           check(board,word,i,j+1,k+1,visited))
+        {
+            return true;
+        } //right
         visited[i][j]=false;
+        return false;
     }
 }
