0144307f8b1535d3c12fc0435db43fcb6eeb6f0a
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2026-09-09 05:51:34 +0530

    Time: 11 ms (96.30%) | Memory: 51.8 MB (5.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int x=nums.length;
-        List<Integer> freq[]=new List[x+1];
         HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<x;i++){
+        List<Integer> freq[]=new List[nums.length+1];
+        for(int i=0;i<nums.length;i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-
-        for(int i=0;i<=x;i++){
+        for(int i=0;i<freq.length;i++){
             freq[i]=new ArrayList<>();
         }
-
-        for(Map.Entry<Integer,Integer> entry: hm.entrySet()){
+        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
             freq[entry.getValue()].add(entry.getKey());
         }
         int j=0;
         int ans[]=new int[k];
         for(int i=freq.length-1;i>=0 && j<=k;i--){
-            for(int n:freq[i]){
+            for(int n: freq[i]){
                 ans[j++]=n;
                 if(j==k){
                     return ans;
